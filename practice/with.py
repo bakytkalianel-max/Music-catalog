2c064287cefc8014cfc55e66bb6ade13198f4227
Anel <you@example.com>
2026-02-12 10:05:19 +0500

    add ptactice directory

---
+++ practice/with.py
@@ -0,0 +1,45 @@
+with open("data.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
+
+with open("file.txt", "w", encoding="utf-8") as f:
+    for i in range(1,11):
+        f.write(str(i) + "\n")
+
+with open("file.txt", "r", encoding="utf-8") as f:
+    print(f.read())
+
+with open("names.txt", "w", encoding="utf-8") as f:
+    f.write("anel\n")
+    f.write("sholpan\n")
+    f.write("amina\n")
+
+with open("names.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        print(line.strip().capitalize())
+
+import csv
+data = [
+    ['ID', 'Товар', 'Цена'],
+    [1, 'Клавиатура', 1500],
+]
+with open("comp.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+
+import csv
+nums= range(1,11)
+with open("comp.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+
+    for num in nums:
+        writer.writerow([num])
+
+import csv
+names=["anel","samat","aisulu"]
+with open("comp.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+    for name in names:
+        writer.writerow([
+            name,
+            name.capitalize()])
