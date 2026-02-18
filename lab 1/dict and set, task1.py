8924072257c6451e8b7c02a6c9eb9fc244d7d3e8
Anel <you@example.com>
2026-02-18 19:13:57 +0500

    task 3 completed

--- lab 1/dict and set, task1.py
+++ lab 1/dict and set, task1.py
@@ -17,3 +17,18 @@ filter_numbers = lambda s: {
 }
 nums = {1, 3, 5, 7, 9, 11, 20, 25}
 print(filter_numbers(nums))
+
+#3
+def merge_dicts_sum(d1, d2):
+    result = {}
+    for key in d1:
+        result[key] = d1[key]
+    for key in d2:
+        if key in result:
+            result[key] += d2[key]
+        else:
+            result[key] = d2[key]
+    return result
+a = {"x": 1, "y": 2}
+b = {"y": 3, "z": 4}
+print(merge_dicts_sum(a, b))
