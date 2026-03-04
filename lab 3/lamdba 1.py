1df468428ea9e1541e330df9199ab1aa3eb163c2
Anel <you@example.com>
2026-03-04 16:42:50 +0500

    task 3

--- lab 3/lamdba 1.py
+++ lab 3/lamdba 1.py
@@ -8,3 +8,8 @@ print(check(0))
 words = ["арбуз", "кот", "машина", "дом", "ананас"]
 sorted_words = sorted(words, key=lambda w: (len(w),w))
 print(sorted_words)
+
+#3
+numbers = [5, 12, 7, 20, 33, 8]
+filter_numbers = list(filter(lambda n: n>10 and n%2==0, numbers))
+print(filter_numbers)
