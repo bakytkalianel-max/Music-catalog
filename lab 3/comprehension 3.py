fb7044cd207ab107f4a16bcd0d3e54ac3e37df51
Anel <you@example.com>
2026-03-04 19:21:26 +0500

    task 13

--- lab 3/comprehension 3.py
+++ lab 3/comprehension 3.py
@@ -7,3 +7,8 @@ import math
 matrix = [[1,2,3], [4,5,6], [7,8,9]]
 a=[(lambda row: math.prod(row))(row) for row in matrix]
 print(a)
+
+#3
+words = ["кот", "машина", "ананас", "дом", "спрей"]
+a=[w for w in words if len(w)>4 and "а" not in w]
+print(a)
