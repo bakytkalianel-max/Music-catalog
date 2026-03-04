a25f5b49287accf4710a92cb6ec0bdc59dc9775b
Anel <you@example.com>
2026-03-04 19:15:36 +0500

    task 12

--- lab 3/comprehension 3.py
+++ lab 3/comprehension 3.py
@@ -1,3 +1,9 @@
 #1
 even=[x**2 for x in range(1,21) if x%2==0]
 print(even)
+
+#2
+import math
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+a=[(lambda row: math.prod(row))(row) for row in matrix]
+print(a)
