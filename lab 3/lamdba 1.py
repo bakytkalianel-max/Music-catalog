904fe9984e39a29656017ca76953b23faa56063c
Anel <you@example.com>
2026-03-04 16:46:05 +0500

    task 4

--- lab 3/lamdba 1.py
+++ lab 3/lamdba 1.py
@@ -13,3 +13,8 @@ print(sorted_words)
 numbers = [5, 12, 7, 20, 33, 8]
 filter_numbers = list(filter(lambda n: n>10 and n%2==0, numbers))
 print(filter_numbers)
+
+#4
+numbers = [1, 2, 3, 4, 5, 6]
+nums=list(map(lambda x: x**2 if x%2==0 else x*3, numbers))
+print(nums)
