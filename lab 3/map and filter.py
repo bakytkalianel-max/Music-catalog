40eb8afa91387bdb59c1a4a1368b27f71e59cd23
Anel <you@example.com>
2026-03-11 17:19:23 +0500

    task 25

--- lab 3/map and filter.py
+++ lab 3/map and filter.py
@@ -12,3 +12,9 @@ print(result)
 nums=[11,12,5,7,8,10,26]
 result=list(filter(lambda x: x%2==0, nums))
 print(result)
+
+#4
+nums=[1,5,-6,7,-2,4,12,24]
+result=list(map(lambda x: x/2 if x%2==0 else x*3,
+                filter(lambda x:x>5,nums)))
+print(result)
