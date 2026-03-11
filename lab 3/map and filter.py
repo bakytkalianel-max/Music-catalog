1b7b1c234e245959ff97a335b0de0a8ccd10eaa7
Anel <you@example.com>
2026-03-11 17:11:54 +0500

    task 23

--- lab 3/map and filter.py
+++ lab 3/map and filter.py
@@ -2,3 +2,8 @@
 nums = [4, 5, 6, 7, 8]
 result=list(map(lambda x: x*2, nums))
 print(result)
+
+#2
+word=["anel", "car", "safe", "apple"]
+result=list(map(lambda x:x.upper() + "!" if len(x)>3 else x.upper(), word ))
+print(result)
