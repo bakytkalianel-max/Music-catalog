9576785a26f3c99cd533aecbf2f0736816b0ac37
Anel <you@example.com>
2026-03-26 08:36:58 +0500

    task 2

--- lab 4/task 1.py
+++ lab 4/task 1.py
@@ -11,3 +11,31 @@ class Player:
 
 p = Player(1, " john ", 120)
 print(p)
+
+#2
+class Player:
+    def __init__(self,_id,name,hp):
+        self._id = _id
+        self._name = name.strip().title()
+        self._hp = hp if hp>=0  else 0
+    def __str__(self):
+        return f"Player(id={self._id},name={self._name},hp={self._hp})"
+    def __del__(self):
+        print(f"Player {self._name} удалён")
+
+    @classmethod
+    def from_string(cls, data: str):
+        parts = data.split(",")
+        parts = [x.strip() for x in parts]
+        if len(parts) != 3:
+            raise ValueError
+
+        _id = int(parts[0])
+        name = parts[1]
+        hp = int(parts[2])
+
+        return cls(_id, name, hp)
+
+p = Player.from_string("2, alice , 90")
+print(p)
+
