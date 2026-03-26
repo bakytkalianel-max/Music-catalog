624a9891d833f06da1442748d6ae027e4593c2e4
Anel <you@example.com>
2026-03-26 09:24:55 +0500

    task 5

--- lab 4/игроки и предметы.py
+++ lab 4/игроки и предметы.py
@@ -81,3 +81,31 @@ inv.add_item(i1)
 inv.add_item(i2)
 inv.add_item(i3)
 print(len(inv.get_items()))
+
+#5
+class Inventory:
+    def __init__(self):
+        self.items = []
+    def add_item(self,item):
+        for i in self.items:
+            if i.id==item.id:
+                return
+        self.items.append(item)
+    def remove_item(self,item_id):
+        self.items = [i for i in self.items if i.id==item_id]
+    def get_items(self):
+        return self.items
+    def unique_items(self):
+        return set(self.items)
+    def to_dict(self):
+        return {item.id: item for item in self.items}
+    def get_strong_items(self,min_power):
+        return list(filter(lambda item: item.power>=min_power, self.items))
+
+inv = Inventory()
+
+inv.add_item(Item(id=1, name = "Sword",power = 50))
+inv.add_item(Item(id=2, name="Shield", power=30))
+inv.add_item(Item(id=3, name="Axe",power= 70))
+
+print(inv.get_strong_items(50))
