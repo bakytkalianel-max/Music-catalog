bf0e4323dffaa10e4341a915bb71cfa583fd6ce7
Anel <you@example.com>
2026-04-08 19:17:26 +0500

    task 13

--- lab 5/Numpy.py
+++ lab 5/Numpy.py
@@ -12,3 +12,11 @@ def get_price_array(prices):
 prices= np.array([1200.0, 25.0, 450.0])
 a=get_price_array(prices)
 print(a)
+#13
+def norm_prices(prices):
+    min_p= np.min(prices)
+    max_p= np.max(prices)
+    return (prices - min_p) / (max_p - min_p)
+prices= np.array([1200.0, 25.0, 450.0])
+a=norm_prices(prices)
+print(a)
