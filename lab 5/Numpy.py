435f8ab8b59a620a58e68b7bf4968ecb200cf378
Anel <you@example.com>
2026-04-08 19:13:19 +0500

    task 12

--- lab 5/Numpy.py
+++ lab 5/Numpy.py
@@ -1,5 +1,14 @@
 #11
 import numpy as np
+
 def price_array(products):
     prices = [product.price for product in products]
-    return np.array(prices, dtype=float)
+    return np.array(prices)
+#12
+def get_price_array(prices):
+    mean_price = float(np.mean(prices))
+    median_price = float(np.median(prices))
+    return (mean_price, median_price)
+prices= np.array([1200.0, 25.0, 450.0])
+a=get_price_array(prices)
+print(a)
