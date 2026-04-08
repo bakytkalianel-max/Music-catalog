0239d09335683d4257f1ba1ea590c0838f328ac9
Anel <you@example.com>
2026-04-08 15:31:22 +0500

    task 2

--- lab 5/1-45.py
+++ lab 5/1-45.py
@@ -11,6 +11,13 @@ class User:
         return f"User(id={self._id}, name={self._name}, email={self._email})"
     def __del__(self):
         print(f"User {self._name} deleted")
-u = User(1, " john doe ", "John@Example.COM")
+#2
+    @classmethod
+    def from_string(cls, data: str):
+        parts = data.split(",")
+        if len(parts) != 3:
+            raise ValueError("Invalid data format")
+        id_str, name, email = parts
+        return cls(int(id_str.strip()), name.strip(), email.strip())
+u = User.from_string("2, Alice Wonderland , alice@wonder.com")
 print(u)
-
