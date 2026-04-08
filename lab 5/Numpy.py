be95b96c96ee6ea3f532ada0928d9e01af7adb48
Anel <you@example.com>
2026-04-08 19:23:32 +0500

    task 14

--- lab 5/Numpy.py
+++ lab 5/Numpy.py
@@ -20,3 +20,21 @@ def norm_prices(prices):
 prices= np.array([1200.0, 25.0, 450.0])
 a=norm_prices(prices)
 print(a)
+
+#14
+import numpy as np
+
+class Product:
+    def __init__(self, id, name, price, category):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.category = category
+def get_categories(products):
+    categories = [product.category for product in products]
+    return np.array(categories)
+products = [
+    Product(1,"Laptop",1200.0,"Electronics"),
+    Product(2,"T-Shirt",20.0,"Clothing")
+]
+print(get_categories(products))
