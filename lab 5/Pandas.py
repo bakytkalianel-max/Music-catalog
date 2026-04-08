a1fe435a5cf6fe1599d32a0a832a24c705a58869
Anel <you@example.com>
2026-04-08 19:59:49 +0500

    task 22

--- lab 5/Pandas.py
+++ lab 5/Pandas.py
@@ -12,3 +12,27 @@ def users_dateframe(users):
             "registration_date": user.registration_date
         })
     return pd.DataFrame(data)
+#22
+import pandas as pd
+class Product:
+    def __init__(self,id, name, price, category):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.category = category
+
+def product_dataframe(products):
+    data=[]
+    for product in products:
+        data.append({
+            "id": product.id,
+            "name": product.name,
+            "category": product.category,
+            "price": product.price
+        })
+    return pd.DataFrame(data)
+products = [
+    Product(1,"Laptop",1200.0,"Electronics"),
+    Product(2,"T-Shirt",20.0,"Clothing")
+]
+print(product_dataframe(products))
