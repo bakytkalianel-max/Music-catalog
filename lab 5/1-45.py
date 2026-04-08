faae7ae0fdd0bb041cd49b25f2a89d67abbc8aa7
Anel <you@example.com>
2026-04-08 15:42:57 +0500

    task 3

--- lab 5/1-45.py
+++ lab 5/1-45.py
@@ -21,3 +21,26 @@ class User:
         return cls(int(id_str.strip()), name.strip(), email.strip())
 u = User.from_string("2, Alice Wonderland , alice@wonder.com")
 print(u)
+
+#3
+class Product:
+    def __init__(self, id: int, name: str, price: float, category: str):
+        self._id = id
+        self._name = name
+        self._price = price
+        self._category = category
+    def __str__(self):
+        return f"Product(id={self._id}, name='{self._name}', price={self._price}, category='{self._category}')"
+    def __eq__(self, other): #сравнение
+        if not isinstance(other, Product):
+            return False
+        return self._id == other._id
+    def __hash__(self):
+        return hash(self._id)
+    def to_dict(self):
+        return {
+            "id": self._id,
+            "name": self._name,
+            "price": self._price,
+            "category": self._category
+        }
