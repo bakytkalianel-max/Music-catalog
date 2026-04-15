0b19b768be0e78b523b5005295c05dfc74ceeaa5
Anel <you@example.com>
2026-04-15 16:39:34 +0500

    task 32

--- lab 5/pandas 31-45.py
+++ lab 5/pandas 31-45.py
@@ -10,3 +10,17 @@ def add_quantity(df):
     df["quantity"] = 1
     return df
 print(add_quantity(df))
+
+#32
+import pandas as pd
+
+df = pd.DataFrame({
+    "order_id": [101, 102],
+    "product_name": ["Laptop", "Mouse"],
+    "price": [1200, 25],
+    "quantity": [2, 3]
+})
+def add_total_price(df):
+    df["total_price"] = df["quantity"] * df["price"]
+    return df
+print(add_total_price(df))
