13970e2859d20654a71b3dca21ba72ba0b0ae1e2
Anel <you@example.com>
2026-05-26 17:43:46 +0500

    task 3,4

--- music catalog project/student 1.py
+++ music catalog project/student 1.py
@@ -1,5 +1,6 @@
 #1
 import pandas as pd
+import numpy as np
 df = pd.read_excel("music_catalog.xlsx")
 print("Первые 5 строк:")
 print(df.head())
@@ -17,3 +18,17 @@ print("\nУникальные жанры:")
 print(genre_list)
 print("\nУникальные ключи:")
 print(key_list)
+
+#3
+upper_titles = df["Title"].str.upper().tolist()
+title_lengths = df["Title"].str.len().tolist()
+mean_length = np.mean(title_lengths)
+print(f"Орташа атау ұзындығы: {mean_length:.2f}")
+
+#4
+top_tracks = df[
+    (df["User_Rating"] >= 8) &
+    (df["Streams_million"] >= 100)
+]
+print("\nТоп популярных треков:")
+print(top_tracks[["Title","Artist","Streams_million","User_Rating"]].head(10))
